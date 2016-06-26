adb8ca2205b152da071f439d6c39d62b6d918e17
KingofGamesYami <KingofGamesYami@users.noreply.github.com>
2016-06-26 15:26:08 -0500

    Update ghost.lua

--- ghost.lua
+++ ghost.lua
@@ -56,18 +56,11 @@ function merge(t1, t2) --http://stackoverflow.com/a/7470789
     return t1
 end
 
-local tTurtleList = {
-	["computercraft:CC-TurtleExpanded"] = true,
-	["computercraft:CC-TurtleAdvanced"] = true,
-	["computercraft:CC-Turtle"] = true,
-}
-
-
 function scanArea( x1, y1, z1, x2, y2, z2 )
 	local minx, miny, minz = math.min( x1, x2 ), math.min( y1, y2 ), math.min( z1, z2 )
 	local maxx, maxy, maxz = math.max( x1, x2 ), math.max( y1, y2 ), math.max( z1, z2 )
 	local tAllInfo = {}
-	--parallel.waitForAll( function()
+	parallel.waitForAll( function()
 		for ix = minx, maxx, 16 do
 			for iy = miny, maxy, 16 do
 				for iz = minz, maxz, 16 do
@@ -76,23 +69,19 @@ function scanArea( x1, y1, z1, x2, y2, z2 )
 				end
 			end
 		end
-	--end, function()
-		sleep( 1 )
+	end, function()
 		for ix = minx, maxx do
 			for iy = miny, maxy do
 				for iz = minz, maxz do
 					local ok, result = commands.blockdata( ix, iy, iz, {} )
-					if result[ 1 ]:match( "did not change" ) and tAllInfo[ ix ][ iy ][ iz ].name == "minecraft:chest" then
-						print( result[ 1 ] )
-						tAllInfo[ ix ][ iy ][ iz ].blockdata = "{" .. result[ 1 ]:match( "change%: (.+)" ):match( "{x%:%-?%d+,y%:%-?%d+,z%:%-?%d+,(.+),id%:" ) .. "}"
-					elseif result[ 1 ]:match( "did not change" ) and tTurtleList[ tAllInfo[ ix ][ iy ][ iz ].name ] then
-						local str = result[ 1 ]:gsub( "[xyz]:%-?%d+,", "" )
-						tAllInfo[ ix ][ iy ][ iz ].blockdata = str
-					end
+          if result[ 1 ]:match( "did not change" ) then
+            tAllInfo[ ix ][ iy ][ iz ].blockdata = result[ 1 ]:match( "change: (.+)" ):gsub( "[xyz]:%-?%d+,", "" )
+          end
+          commands.setblock( ix, iy, iz, "minecraft:air" )
 				end
 			end
 		end
-	--end )
+	end )
 	return tAllInfo
 end
 
@@ -101,3 +90,12 @@ local file = fs.open( ".ghost", "w" )
 file.write( textutils.serialize( info ) )
 file.close()
 
+sleep( 1 )
+
+for ix, t in pairs( info ) do
+  for iy, t2 in pairs( t ) do
+    for iz, info in pairs( t2 ) do
+      commands.setblock( ix, iy, iz, info.name, info.metadata, "replace", info.blockdata )
+    end
+  end
+end
