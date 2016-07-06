28abf0f31778ae1c9e8ac17f74adccfaf909c74f
KingofGamesYami <KingofGamesYami@users.noreply.github.com>
2016-07-06 09:11:30 -0500

    idek if this will help anything but apparently my version was different so... work dammit

--- async.lua
+++ async.lua
@@ -10,14 +10,13 @@ local tRunningIds = {}
 
 function run()
 	while true do
-		if nRunningCommands < 500 and #tCommandsToExecute > 0 then
+		while nRunningCommands < 100 and #tCommandsToExecute > 0 do
 			tRunningIds[ commands.execAsync( table.remove( tCommandsToExecute, 1 ) ) ] = true
-		else
-			local event = {os.pullEvent()}
-			if event[ 1 ] == "task_complete" and tRunningIds[ event[ 2 ] ] then
-				tRunningIds[ event[ 2 ] ] = false
-				nRunningCommands = nRunningCommands - 1
-			end
+		end
+		local event = {os.pullEvent()}
+		if event[ 1 ] == "task_complete" and tRunningIds[ event[ 2 ] ] then
+			tRunningIds[ event[ 2 ] ] = false
+			nRunningCommands = nRunningCommands - 1
 		end
 	end
 end
